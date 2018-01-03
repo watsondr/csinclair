71bb47bbe5d1e645338cf6cf041a27cf82d83d19
Campbell Sinclair <campbell.sinc@gmail.com>
2018-01-03 18:31:21 +1100

    moved to font-awesome version 5.0.2

--- header.php
+++ header.php
@@ -1,13 +1,3 @@
-		<!--
-
-		*******************************
-		
-		THIS IS THE MOBILE VERSION
-		
-		*******************************	
-		
-		-->
-
 		<meta charset="UTF-8">
 		<meta name="viewport" content="initial-scale=1" />
 		
@@ -18,8 +8,10 @@
 		<!-- for mobile -->
 		<link rel="stylesheet" media="screen and (max-width: 1023px)" href="/css/mainMobile.css"  />
 		
-		<script src="https://use.fontawesome.com/00bad2207d.js"></script>
+		<!-- <script src="https://use.fontawesome.com/00bad2207d.js"></script> -->
+		<!-- <script defer src="https://use.fontawesome.com/releases/v5.0.2/js/all.js"></script> -->
 		<link href="https://fonts.googleapis.com/css?family=Droid+Sans|Fira+Sans" rel="stylesheet"> 
 
+		<script defer src="/js/fontawesome-all.js"></script>
 		<script src="/js/jquery-3.2.1.min.js"></script>
-	    <script type="text/javascript" src="/js/nav.js"></script>
+	    <script src="/js/nav.js"></script>
