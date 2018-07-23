8d74ac8aea4964af32e37dd05de5dfd29323fb6b
Campbell Sinclair <watsondr@users.noreply.github.com>
2018-07-23 21:27:55 +1000

    CSS refactoring begin
    prep work to make managable media varients of layout and use proper css variables.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # csinclair
 Example git for my website.
-I am not uploading any images used as I don't expect anyone else to use this. The repository is for demonstration of skills.
+I am not uploading any images used as I don't expect anyone else to use this. The repository is for demonstration of skills,
+and transperency regarding evolution of my work.
--- header.php
+++ header.php
@@ -7,6 +7,8 @@
 		<link rel="stylesheet" media="screen and (min-width: 1024px)" href="/css/main.css"  />
 		<!-- for mobile -->
 		<link rel="stylesheet" media="screen and (max-width: 1023px)" href="/css/mainMobile.css"  />
+		<!-- incoming -->
+		<link rel="stylesheet" media="screen" href="/css/root_media_edt.css" />
 		
 		<!-- <script src="https://use.fontawesome.com/00bad2207d.js"></script> -->
 		<!-- <script defer src="https://use.fontawesome.com/releases/v5.0.2/js/all.js"></script> -->
