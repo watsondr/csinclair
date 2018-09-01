518d42d923515eb5fbf244da540c240b7e0aedff
Campbell Sinclair <watsondr@users.noreply.github.com>
2018-09-01 21:53:31 +1000

    added new project

--- portfolio.php
+++ portfolio.php
@@ -29,13 +29,13 @@
 				<div class="portfolioBox">
 						<div class="front">
 							<p>
-								<a href="https://codepen.io/Watsonian/pen/gLmyQv">
+								<a href="https://codepen.io/Watsonian/pen/gLmyQv" target='_blank'>
 									<i class="fab fa-wikipedia-w fa-9x" aria-hidden="true"></i>
 								</a>
 							</p>
 						</div>
 						<div class="back">
-								<a href="https://codepen.io/Watsonian/pen/gLmyQv">							
+								<a href="https://codepen.io/Watsonian/pen/gLmyQv" target='_blank'>							
 									Wikipedia's RestAPI for search results, using JSON return to show descriptions with URL links.
 								</a>
 						</div>
@@ -45,13 +45,13 @@
 				<div class="portfolioBox">
 						<div class="front">
 							<p>
-								<a href="https://codepen.io/Watsonian/pen/ObZQXr">
+								<a href="https://codepen.io/Watsonian/pen/ObZQXr" target='_blank'>
 									<i class="fab fa-twitch fa-9x" aria-hidden="true"></i>
 								</a>
 							</p>
 						</div>
 						<div class="back">
-								<a href="https://codepen.io/Watsonian/pen/ObZQXr">							
+								<a href="https://codepen.io/Watsonian/pen/ObZQXr" target='_blank'>							
 									I use Twitch.tv's RestAPI to show who is online / offline on their website out of pre-defined users.
 								</a>
 						</div>
@@ -61,17 +61,33 @@
 				<div class="portfolioBox">
 						<div class="front">
 							<p>	
-								<a href="https://codepen.io/Watsonian/pen/WxvgRY">
+								<a href="https://codepen.io/Watsonian/pen/WxvgRY" target='_blank'>
 									<i class="fas fa-anchor fa-9x" aria-hidden="true"></i>
 								</a>
 							</p>
 						</div>
 						<div class="back">
-							<a href="https://codepen.io/Watsonian/pen/WxvgRY">
+							<a href="https://codepen.io/Watsonian/pen/WxvgRY" target='_blank'>
 								This is an example of a landing page with navigational anchor points.
 							</a>
 						</div>
-				</div>				
+				</div>		
+
+				<!-- Box 4 -->
+				<div class="portfolioBox">
+						<div class="front">
+							<p>	
+							<a href="https://codepen.io/Watsonian/pen/GxvaPq" target='_blank'>
+									<i class="fas fa-calculator fa-9x" aria-hidden="true"></i>
+								</a>
+							</p>
+						</div>
+						<div class="back">
+							<a href="https://codepen.io/Watsonian/pen/GxvaPq" target='_blank'>
+								This calculator is an example of interactive client side JavaScript application.
+							</a>
+						</div>
+				</div>								
 				
 			</div>
 			
